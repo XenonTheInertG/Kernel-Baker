66f52ef548931fa936b5d8e33fc717cb1be9ea87
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-03-14 00:03:53 +0600

    Update and rename Inertia.sh to Huge_Tree.sh

--- Base/Huge_Tree.sh
+++ Base/Huge_Tree.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# 2020 (c) Noblepie/XenonTheInertG
+# 2021 (c) Noble-Kernel/XenonTheInertG
 
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b $BRNCH packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_device_qcom_commom -b $BRNCH frvice/qcom/common
 git clone https://github.com/LineageOS/android_device_cyanogen_msm8916-common -b $BRNCH frvice/cyanogen/msm8916-common
+(add your device common tree here,you can find it github? Don't have device tree? that's not a problem, watch my Roms & kernels compliation;)
 
