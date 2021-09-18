5ec3f70c8ea54a0b9b85e7d4e37394020b3fe62f
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-09-18 10:29:32 +0600

    backend:removed unusual stuffs

--- Base/Huge_Tree.sh
+++ Base/Huge_Tree.sh
@@ -4,5 +4,3 @@
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b $BRNCH packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_device_qcom_commom -b $BRNCH frvice/qcom/common
 git clone https://github.com/LineageOS/android_device_cyanogen_msm8916-common -b $BRNCH frvice/cyanogen/msm8916-common
-(add your device common tree here,you can find it github? Don't have device tree? that's not a problem, watch my Roms & kernels compliation;)
-
